00ea18c530baf5ad87aea4161bfce3d5d71ae25b
isaac <isaach2026@student.cis.edu.hk>
2023-11-16 12:01:18 +0800

    test done

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,20 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
+        int[] combinedArray = new int[array1.length + array2.length];
 
-        return null;
+        int index = 0;
+        for (int num : array1) {
+            combinedArray[index] = num;
+            index++;
+        }
+
+        for (int num : array2) {
+            combinedArray[index] = num;
+            index++;
+        }
+
+        return combinedArray;
 
     }
 
@@ -21,8 +33,16 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
+        int[]zippedArray = new int[array1.length * 2];
 
-        return null;
+        int index = 0;
+        for (int i = 0; i < array1.length; i++) {
+            zippedArray[index] = array1[i];
+            index++;
+            zippedArray[index] = array2[i];
+            index++;
+        }
+        return zippedArray;
 
     }
 
@@ -34,8 +54,13 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
+        int[] productArray = new int[array1.length];
 
-        return null;
+        for (int i = 0; i < array1.length; i++) {
+            productArray[i] = array1[i] * array2[i];
+        }
+
+        return productArray;
 
     }
 
@@ -56,15 +81,25 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
+        int[] capitalCounts = new int[words.length];
 
-        return null;
+        for (int i = 0; i < words.length; i++) {
+            capitalCounts[i] = countCapitalLetters(words[i]);
+        }
 
+        return capitalCounts;
     }
 
     public static int countCapitalLetters(String word) {
+        int count = 0;
 
-        return 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (Character.isAlphabetic(c) && Character.isUpperCase(c)) {
+                count++;
+            }
+        }
 
+        return count;
     }
-
-}
+    }
